1bc502a855682e9795e0c8596c65015f95b3e0a0
GODSPE1 <godspy18@gmail.com>
2024-07-24 23:24:11 +0100

    pycodestyle style documentation

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -2,10 +2,12 @@
 """This module defines a LRUCache class"""
 from base_caching import BaseCaching
 
+
 class LRUCache(BaseCaching):
     """A basic caching system that follows the LRU algorithm"""
     def __init__(self):
-        """Initialize the LRUCache class by calling the parent class's __init__ method"""
+        """Initialize the LRUCache class by calling the parent
+        class's __init__ method"""
         super().__init__()
 
     def move_to_end(self, key):
@@ -15,9 +17,9 @@ class LRUCache(BaseCaching):
 
     def put(self, key, item):
         """
-        Assign the item value to the key in the cache data dictionary.
-        If the cache is full, discard the least recently used item (LRU algorithm).
-        """
+        Assign the item value to the key in the cache data
+        dictionary. If the cache is full, discard the least
+        recently used item(LRU algorithm)."""
         if key is not None and item is not None:
             if key in self.cache_data:
                 self.move_to_end(key)
@@ -28,7 +30,8 @@ class LRUCache(BaseCaching):
             self.cache_data[key] = item
 
     def get(self, key):
-        """Return the value associated with the given key in the cache data dictionary"""
+        """Return the value associated with the given key in
+        the cache data dictionary"""
         if key in self.cache_data and key is not None:
             self.move_to_end(key)
             return self.cache_data[key]
