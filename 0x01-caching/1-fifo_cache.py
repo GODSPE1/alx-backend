36bd59b613b8346893f9d4f093c7b2fddc0ee829
GODSPE1 <godspy18@gmail.com>
2024-07-24 14:35:03 +0100

    corrected

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -16,19 +16,18 @@ class FIFOCache(BaseCaching):
         Assign the item value to the key in the cache data dictionary.
         If the cache is full, discard the oldest item (FIFO algorithm).
         """
-        if key is None and item is None:
-            return None
-        else len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            discard = next(iter(self.cache_data))
-            del self.cache_data[discard]
-            print("DISCARD:", discard)
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discard = next(iter(self.cache_data))
+                del self.cache_data[discard]
+                print("DISCARD:", discard)
             self.cache_data[key] = item
 
     def get(self, key):
         """Return the value associated with the
         given key in the cache data dictionary
         """
-        if key is None and key not in self.cache_data:
-            return None
-        else:
+        if key in self.cache_data and key is not None:
             return self.cache_data[key]
+        else:
+            return None
