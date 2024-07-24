eeb38d640e073af37b1f667c6351f63adcf82910
GODSPE1 <godspy18@gmail.com>
2024-07-24 13:59:03 +0100

    implemented FIFO algorithm

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""This module defines a FIFOCache class"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """A basic caching system that follows the FIFO algorithm"""
+    def __init__(self):
+        """Initialize the FIFOCache class by calling
+        the parent class's __init__ method
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Assign the item value to the key in the cache data dictionary.
+        If the cache is full, discard the oldest item (FIFO algorithm).
+        """
+        if key is None or item is None:
+            return None
+        elif len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            discard = next(iter(self.cache_data))
+            del self.cache_data[discard]
+            print("DISCARD:", discard)
+            self.cache_data[key] = item
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Return the value associated with the
+        given key in the cache data dictionary
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        else:
+            return self.cache_data[key]
