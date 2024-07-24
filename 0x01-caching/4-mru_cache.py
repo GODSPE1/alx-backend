438d469aab25c0ff052aa274be7fa84607cb390b
GODSPE1 <godspy18@gmail.com>
2024-07-25 00:46:29 +0100

    used reversed method

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -11,22 +11,30 @@ class MRUCache(BaseCaching):
         class's __init__ method"""
         super().__init__()
 
+    def move_to_end(self, key):
+        """Move the specified key to the end of the dictionary"""
+        value = self.cache_data.pop(key)
+        self.cache_data[key] = value
+
     def put(self, key, item):
         """
         Assign the item value to the key in the cache data dictionary.
         If the cache is full, discard the most recently used
         item (MRU algorithm).
         """
-        if key is None and item is None:
-            return
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            if key in self.cache_data:
-                self.cache_data.popitem()
-        self.cache_data[key] = item
+        if key is not None or item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                if key not in self.cache_data:
+                    discard = next(reversed(self.cache_data))
+                    del self.cache_data[discard]
+                    #removed = self.cache_data.popitem()
+                    print("DISCARD:", discard)
+            self.cache_data[key] = item
 
     def get(self, key):
         """Return the value associated with the given key in
         the cache data dictionary"""
         if key is not None and key in self.cache_data:
+            self.move_to_end(key)
             return self.cache_data[key]
         return None
