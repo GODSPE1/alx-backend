8739df1077709817b5c9770f013272a8fb4f1b2a
GODSPE1 <godspy18@gmail.com>
2024-07-26 23:51:48 +0100

    type annotated the function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,9 +2,10 @@
 This module defines a function named index_range that takes two
 integer arguments page and page_size
 """
+from typing import Tuple
 
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Return a tuple of size two containing a start index and an end index"""
     if page <= 0:
         return (0, 0)
