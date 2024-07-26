873a7a1702187b72ae879fc0e06f75e44040fcb9
GODSPE1 <godspy18@gmail.com>
2024-07-26 23:55:41 +0100

    changed the implementation

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -7,9 +7,6 @@ from typing import Tuple
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Return a tuple of size two containing a start index and an end index"""
-    if page <= 0:
-        return (0, 0)
-    else:
-        start_index = (page - 1) * page_size
-        end_index = start_index + page_size
-        return (start_index, end_index)
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
