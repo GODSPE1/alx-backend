75fc616b57d9f6f52b34251b5d7cfbec8d231490
GODSPE1 <godspy18@gmail.com>
2024-07-26 16:07:14 +0100

    function return the index range of  PAGE

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,8 +1,14 @@
-"""This module defines a function named index_range that takes two integer arguments page and page_size"""
+"""
+This module defines a function named index_range that takes two
+integer arguments page and page_size
+"""
 
 
 def index_range(page, page_size):
     """Return a tuple of size two containing a start index and an end index"""
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-    return (start_index, end_index)
+    if page <= 0:
+        return (0, 0)
+    else:
+        start_index = (page - 1) * page_size
+        end_index = start_index + page_size
+        return (start_index, end_index)
