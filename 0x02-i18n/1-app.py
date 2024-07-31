1a3a9c5be866246b7b1fda682cf9ef3d12c561fb
GODSPE1 <godspy18@gmail.com>
2024-07-31 04:43:54 +0100

    a basic babel setup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -3,21 +3,23 @@
 from flask import Flask, render_template
 from flask_babel import Babel
 
+
 class Config:
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
-babel =  Babel(app)
+babel = Babel(app)
+
 
 @app.route('/')
 def index():
     """simply outputs “Welcome to Holberton” as page title (<title>)
     and “Hello world” as header (<h1>)"""
     return render_template('0-index.html', title='Welcome to Holberton')
-
 
 
 if __name__ == '__main__':
