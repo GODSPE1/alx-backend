0791921a0996348d54f523530838a8bb07cb3e54
GODSPE1 <godspy18@gmail.com>
2024-07-31 04:59:08 +0100

    decorator function to select language

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""This module defines a basic babel setup app"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Get locale fro a request"""
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+@app.route('/')
+def index():
+    """simply outputs “Welcome to Holberton” as page title (<title>)
+    and “Hello world” as header (<h1>)"""
+    return render_template('0-index.html', title='Welcome to Holberton')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
