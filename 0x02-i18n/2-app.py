eb29f650ca9f149b43aca71cae3324b4c8f8a59d
GODSPE1 <godspy18@gmail.com>
2024-07-31 09:28:34 +0100

    refactored

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """This module defines a basic babel setup app"""
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babel import Babel, _
 
 
 class Config:
@@ -12,20 +12,21 @@ class Config:
 
 app = Flask(__name__)
 app.config.from_object(Config)
-babel = Babel(app)
 
 
-@babel.localeselector
-def get_locale():
-    """Get locale fro a request"""
+def get_locale() -> str:
+    """Get locale from a request"""
     return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+babel = Babel(app, locale_selector=get_locale)
 
 
 @app.route('/')
 def index():
     """simply outputs “Welcome to Holberton” as page title (<title>)
     and “Hello world” as header (<h1>)"""
-    return render_template('0-index.html', title='Welcome to Holberton')
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
